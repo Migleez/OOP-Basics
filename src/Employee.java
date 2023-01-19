94137d9a551140aa51633bc57b15fdde0b66c21d
Migleez <migle.kaltenyte@gmail.com>
2023-01-19 12:51:38 +0200

    first commit

---
+++ src/Employee.java
@@ -0,0 +1,9 @@
+public class Employee {
+    String name = "Name";
+    String surname = "Surname";
+    String contractType;
+  //          (Full-Time, Part-Time),
+    String contractStartDate = "2022-01-01";
+    float salary = 2000;
+    String position = "Manager";
+}
---
+++ src/EmployeeManager.java
@@ -0,0 +1,9 @@
+public class EmployeeManager {
+
+    public static void main(String[] args) {
+        Main employee = new Main();   // Create a employee object
+        public void fullThrottle() {
+            System.out.println("The car is going as fast as it can!");
+        }
+    }
+}
